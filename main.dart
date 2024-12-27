61cbc5bcb1d6884cee642a81041a7f32774a2fb5
sivachathurya2004 <sivachathurya.p@gmail.com>
2024-12-27 18:19:08 +0530

    FRESH

--- main.dart
+++
@@ -1,5 +0,0 @@
-void main(){
-var firstName = "John";
-var lastName = "Doe";
-print("Full name is $firstName $lastName");
-}
--- process.py
+++
@@ -1,4 +0,0 @@
-with open(r"C:\Chathurya(525)\Registration.html", "r") as file:
-    content = file.read()
-    print("HTML file content:")
-    print(content)
